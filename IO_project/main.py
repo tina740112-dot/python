542e37ff9ea0d7e92f146cbc755efa65de03de7a
tina740112 <tina740112@gmail.com>
2026-09-17 10:46:22 +0800

    update

--- IO_project/main.py
+++ IO_project/main.py
@@ -1,4 +1,7 @@
 import platform#載入platform工具
+##呼叫usb_test 進來
+from usb_test import start_usb_test #從 usb_test.py 匯入 start_usb_test 函式
+
 print("===Peripheral Test Tool===")
 #platform.system()platform的system確認作業系統
 os_name=platform.system()#取得作業系統名稱並存入 os_name
@@ -10,16 +13,19 @@ elif os_name=='Linux':
   print('Linux System Detected')
 else:
   print('Unsupported System')
-#usb test
+################usb test
 try:
   user=int(input('Please select usb option 1/2:'))
   if user==1:
-    print('Starting USB Test')
+    #print('Starting USB Test')
+    start_usb_test()#呼叫usb_test
   elif user==2:
     print('Exit')
   else:
     print('Invalid option')
 except ValueError:
   print('Invalid option')
-    
+
+
+
   
---
+++ IO_project/usb_test.py
@@ -0,0 +1,25 @@
+import subprocess
+def start_usb_test():
+  result=subprocess.run(
+    ['powershell','-Command','get-PnpDevice -PresentOnly -Class USB'],
+    #取得 Windows 的 PnP 裝置
+    # -PresentOnly 只顯示目前存在的裝置
+    # -Class USB 只顯示 USB 類別的裝置
+    capture_output=True,
+    #capture(抓取) output(輸出)PowerShell 的輸出抓回 Python,
+    # True ：要!!開啟這個功能。
+    text=True,#抓回來的東西，我要用「文字」處理。
+    #PowerShell 的輸出被 Python 抓回來之後，可以用不同形式處理。在這裡我們用str輸出。
+    encoding='big5'#指定抓回來的文字編碼為 big5,正確解碼中文字
+    )
+  print('starting USB Test')
+  print(result.stdout)#現在把抓回來的正常輸出拿出來
+  #stdout=standard output
+#start_usb_test()#呼叫並執行函式
+
+
+#練習
+# def show_name(name):
+#   print('name:',name)
+  
+# show_name("amy")
