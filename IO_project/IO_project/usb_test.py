7dc9c78403988f8f64eeeb935220b16402b3cfcb
tina740112 <tina740112@gmail.com>
2026-09-17 10:04:07 +0800

    update

--- IO_project/IO_project/usb_test.py
+++ IO_project/IO_project/usb_test.py
@@ -1,12 +1,19 @@
 import subprocess
 def start_usb_test():
   result=subprocess.run(
-    ['powershell',","'-Command',","'get-date']
+    ['powershell','-Command','get-date'],
     capture_output=True,
-    text=True
+    #capture(抓取) output(輸出)PowerShell 的輸出抓回 Python,True 就是：要!!開啟這個功能。
+    text=True#抓回來的東西，我要用「文字」處理。
+    #PowerShell 的輸出被 Python 抓回來之後，可以用不同形式處理。在這裡我們用str輸出。
     )
   print('starting USB Test')
+  print(result.stdout)
+#start_usb_test()#呼叫並執行函式
 
-#start_usb_test()#呼叫函式
 
-
+#練習
+# def show_name(name):
+#   print('name:',name)
+  
+# show_name("amy")
