df66bd7b37a58822372257df7c37275117cfefd6
tina740112 <tina740112@gmail.com>
2026-09-17 09:12:36 +0800

    update

--- IO_project/IO_project/usb_test.py
+++ IO_project/IO_project/usb_test.py
@@ -1,11 +1,11 @@
 import subprocess
 def start_usb_test():
   result=subprocess.run(
-    ['powershell','-Command','get-date']
-    capture_output=true,
-    text=true
+    ['powershell',","'-Command',","'get-date']
+    capture_output=True,
+    text=True
     )
-print('starting USB Test')
+  print('starting USB Test')
 
 #start_usb_test()#呼叫函式
 
