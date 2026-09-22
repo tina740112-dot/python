4ed8a2c3236f335bdf14774edb1adb4aadad5c9e
tina740112 <tina740112@gmail.com>
2026-09-22 16:18:48 +0800

    add device_result

--- IO_project/usb_test.py
+++ IO_project/usb_test.py
@@ -63,8 +63,16 @@ def start_usb_test():
     print("New USB device detected")
     for new_usb in new_ids:
       print("New USB device detected:", new_usb)
+    command = f'Get-PnpDevice -InstancdId"{new_usb}"'
   else:
     print("no New USB")
+  
+  device_result = subprocess.run(
+    ['powershell','-command','command']
+    capture_output=True,
+    text=True,
+    encoding='big5'
+  )
     
 
   
